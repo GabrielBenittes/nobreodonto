64909a26d1d6201c520c156de9046af579046bc3
Gabriel L. Benites <79459518+GabrielBenittes@users.noreply.github.com>
2023-03-27 22:57:18 -0300

    Add files via upload

---
+++ NobreOdonto/js/scriptmodal.js
@@ -0,0 +1,10 @@
+$('.nav a[href^="#tips"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
+
